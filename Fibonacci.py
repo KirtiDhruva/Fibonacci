6f441bfd51dcda7ca3a38249dcad31fad8c9bef6
Kirti Dhruva <dmwinc2000@yahoo.com>
2020-09-22 18:52:01 +0530

    Commit #1
    Basic Functionality

---
+++ Fibonacci.py
@@ -0,0 +1,89 @@
+"""
+Created on Fri Sep 18 13:09:32 2020
+
+@author: Kirti Dhruva
+"""
+class Fibonacci:
+
+    def __init__(self):
+
+        self.index = 0
+        self.previous = 0
+        self.current = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+
+        self.index += 1
+
+        if self.index == 1:
+            return 0, self.index
+
+        if self.index == 2:
+            return 1, self.index
+
+        temp = self.previous + self.current
+        self.previous = self.current
+        self.current = temp
+        return self.current, self.index
+
+    def reset(self):
+        self.__init__()
+
+    def get_series(self, count, start='current'):
+
+        if start == 'reset':
+            self.reset()
+
+        elif start != 'current':
+            print(f'start: Unknown keyword "{start}"')
+            return
+
+        series = []
+        indexes = []
+
+        count += self.index
+
+        for value, index in self:
+            series.append(value)
+            indexes.append(index)
+            if index == count:
+                break
+        return series, indexes
+
+    def get_series_normalised(self, count, start='current'):
+
+        series, indexes = self.get_series(count, start)
+
+        for i in range(len(series)):
+            series[i] = series[i]/series[-1]
+
+        return series, indexes
+
+
+    def get_ratios1(self, count, start='current'):
+
+        series, indexes = self.get_series(count, start)
+
+        ratios = []
+
+        for i, index in enumerate(indexes[:-1]):
+            ratios.append(series[i]/series[i+1])
+
+        return ratios, indexes
+
+    def get_ratios2(self, count, start='current'):
+
+        series, indexes = self.get_series(count, start)
+
+        ratios = []
+
+        for i, index in enumerate(indexes[:-1]):
+            if index == 1:
+                ratios.append(0)    #could be changes to None
+                continue
+            ratios.append(series[i+1]/series[i])
+
+        return ratios, indexes
---
+++ MyFibonacci.py
@@ -0,0 +1,43 @@
+"""
+Created on Fri Sep 18 12:21:14 2020
+
+@author: dmwin
+"""
+
+from Fibonacci import Fibonacci
+import matplotlib.pyplot as plt
+import numpy as np
+
+terms = 30
+
+f = Fibonacci()
+
+series_norm, _  =  f.get_series_normalised(terms)
+ratios1, _  = f.get_ratios1(terms, 'reset')
+ratios2, indexes = f.get_ratios2(terms, 'reset')
+
+print(series_norm)
+print(ratios1)
+print(ratios2)
+
+line1, = plt.plot(indexes, series_norm, marker='.', color='red', linestyle='', alpha=0.5)
+line2, = plt.plot(indexes[:-1], ratios1, marker='<', color='blue', linestyle='', alpha=0.5)
+line3, = plt.plot(indexes[:-1], ratios2, marker='>', color='green', linestyle='', alpha=0.5)
+
+plt.xticks(range(indexes[0], indexes[-1]+1, 2), rotation=70)
+
+plt.yticks(np.linspace(0, 2, 20), rotation=30)
+plt.title('Fibonacci Stats')
+plt.legend([line1, line2, line3], ['Normalised Series', 'Ratios1', 'Ratios2'])
+plt.grid(linestyle='--')
+plt.show()
+
+stacked = np.vstack([ratios1, ratios2])
+
+std = np.std(stacked, axis=1)
+cov = np.cov(stacked)
+corr = np.corrcoef(stacked)
+
+print(f'Standard Deviation: \n{std}')
+print(f'Covariance-Variance Matrix: \n{cov}')
+print(f'Correlation Coefficient: \n{corr}')
